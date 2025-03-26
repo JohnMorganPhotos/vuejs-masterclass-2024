98d2409807eeef0ee673fbcb1fbe7ecba8ba475c
John Morgan <morgan.johnj@gmail.com>
2025-03-26 11:02:51 +0000

    Working on the tasks migration

--- database/seed.js
+++ database/seed.js
@@ -1,6 +1,40 @@
+/* eslint-env node */
+
 import { faker } from '@faker-js/faker'
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(process.env.VITE_SUPABASE_URL, process.env.SERVICE_ROLE_KEY)
 
-//console.log(supabase)
+const logStep = (stepMessage) => {
+  console.log(stepMessage)
+}
+
+const seedProjects = async (numEntries, userId) => {
+  logStep('Seeding projects...')
+
+  const projects = []
+
+  for (let i = 0; i < numEntries; i++) {
+    const name = faker.lorem.words(3)
+
+    logStep('Seeding project: ' + name)
+
+    projects.push({
+      name: name,
+      slug: name.toLocaleLowerCase().replace(/ /g, '-'), // replace all spaces with dashes
+      description: faker.lorem.paragraphs(2),
+      status: faker.helpers.arrayElement(['in-progress', 'completed']),
+      collaborators: faker.helpers.arrayElements([userId]),
+    })
+  }
+
+  const { data, error } = await supabase.from('projects').insert(projects).select('id')
+
+  // if (error) return logErrorAndExit('Projects', error)
+
+  logStep('Projects seeded successfully.')
+
+  return data
+}
+
+await seedProjects(3, 1)
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -1,9 +1,27 @@
 <!-- eslint-disable vue/multi-word-component-names -->
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { supabase } from '@/lib/supabaseClient'
+import { ref } from 'vue'
+import type { Tables } from '../../../database/types' // Import the type from the database folder
+
+const projects = ref<Tables<'projects'>[] | null>(null)
+;(async () => {
+  const { data, error } = await supabase.from('projects').select('*')
+  if (error) {
+    console.error(error)
+  }
+  // console.log(data)
+  projects.value = data
+  console.log(projects)
+})()
+</script>
 
 <template>
   <div>
     <h1>Project Page</h1>
     <RouterLink to="/">Go To Home</RouterLink>
+    <ul>
+      <li v-for="project in projects" :key="project.id">{{ project.name }}</li>
+    </ul>
   </div>
 </template>
