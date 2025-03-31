e5cd876e3945f27ab73596ce6686ceed4276c960
John Morgan <morgan.johnj@gmail.com>
2025-03-31 16:57:31 +0100

    5.2 unplugin-vue-router

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,18 @@
 import { fileURLToPath, URL } from 'node:url'
+import VueRouter from 'unplugin-vue-router/vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
-import vueRouter from 'unplugin-vue-router/vite'
-import tailwind from 'tailwindcss'
-import autoprefixer from 'autoprefixer'
-import AutoImport from 'unplugin-auto-import/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    vue({
-      template: {
-        compilerOptions: {
-          isCustomElement: (element) => element.startsWith('iconify-icon'),
-        },
-      },
-    }),
-    vueDevTools(),
-    vueRouter(),
+    VueRouter(),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
@@ -27,9 +20,16 @@ export default defineConfig({
         /\.vue\?vue/, // .vue
         /\.md$/, // .md
       ],
-      imports: ['vue', 'vue-router'],
+      imports: ['vue', VueRouterAutoImports],
       dts: true,
       viteOptimizeDeps: true,
+    }),
+    vue({
+      template: {
+        compilerOptions: {
+          isCustomElement: (element) => element.startsWith('iconify-icon'),
+        },
+      },
     }),
   ],
   css: {
@@ -43,5 +43,3 @@ export default defineConfig({
     },
   },
 })
-
-// vite.config.ts
